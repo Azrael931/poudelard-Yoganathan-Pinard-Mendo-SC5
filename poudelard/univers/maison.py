c1583fa047c7ca8060eb647e2be4d8565be73ca4
Romain9294 <romain.pinard@efrei.net>
2025-12-05 19:52:52 +0100

    avancement maison

--- poudelard/univers/maison.py
+++ poudelard/univers/maison.py
@@ -8,8 +8,22 @@ def actualiser_points_maison(maisons, nom_maison, points):
 
     if nom_maison in maisons:
         maisons[nom_maison] += points
-        print(f"La maison {nom_maison} a reçu {points} points.")
-        print(f"Nouveau total : {maisons[nom_maison]}")
+        print(f"La maison {nom_maison} a maintenant {maisons[nom_maison]} points.")
     else:
-        print(f"Erreur : La maison '{nom_maison}' est introuvable.")
+        print(f"Erreur : la maison '{nom_maison}' est introuvable.")
+
+
+'''if __name__ == "__main__":
+    maisons = {
+        "Gryffondor": 50,
+        "Serpentard": 30,
+        "Poufsouffle": 20,
+        "Serdaigle": 40
+    }
+
+    actualiser_points_maison(maisons, "Gryffondor", 10)
+    actualiser_points_maison(maisons, "Serpentard", -5)
+    actualiser_points_maison(maisons, "Poubelle", 15)
+
+    print("État final :", maisons)'''
 
