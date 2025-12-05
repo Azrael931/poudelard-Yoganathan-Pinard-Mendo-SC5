7af31603e05ad39ba729a0684999584d30b7c212
Laurent <thetitan931@gmail.com>
2025-12-05 23:16:54 +0100

    Finilisation de input_utils.py

--- poudelard/utils/input_utils.py
+++ poudelard/utils/input_utils.py
@@ -1,17 +1,16 @@
-'''def demander_texte(message):
+def demander_texte(message):
     while  True:
         texte = input(message).strip()
 
         if len(texte) == 0:
             print("Veuillez entrer un texte non vide.")
         else:
-            return texte'''
+            return texte
 
 def demander_nombre(message, min_val=None, max_val=None):
     while True:
         texte = input(message + " ").strip()
 
-        # Vérifier si la saisie est un nombre
         est_valide = True
         for c in texte:
             if c < '0' or c > '9':
@@ -22,18 +21,32 @@ def demander_nombre(message, min_val=None, max_val=None):
             print("Veuillez entrer uniquement des chiffres.")
             continue
 
-        # Conversion manuelle
         valeur = 0
         for c in texte:
             valeur = valeur * 10 + (ord(c) - ord('0'))
 
-        # Vérification des bornes
         if min_val is not None and valeur < min_val:
-            print(f"Veuillez entrer un nombre >= {min_val}.")
+            print(f"Veuillez entrer un nombre entre {min_val} et {max_val}.")
             continue
 
         if max_val is not None and valeur > max_val:
-            print(f"Veuillez entrer un nombre <= {max_val}.")
+            print(f"Veuillez entrer un nombre {min_val} et {max_val}.")
             continue
 
         return valeur
+
+def demander_choix(message, options):
+    while True :
+
+        for i, option in enumerate(options):
+            print(f"{i+1}. {option}")
+        choix = demander_nombre(message, 1, len(options))
+        return choix
+
+
+if __name__ == "__main__":
+    mes_options = ["Fleur","Gâteau","Viande"]
+    print("---TEST DEMANDER CHOIX---")
+    resultat = demander_choix("Tu préfères quoi entre : ", mes_options)
+    print(f"Tu préfères {mes_options[resultat - 1]}")
+
