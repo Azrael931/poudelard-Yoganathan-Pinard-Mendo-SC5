ead8af8993152a6b3781163afe8d4371f7a0c212
mendo <hans.mendo@efrei.net>
2025-12-05 16:02:27 +0100

    supression fonction

--- poudelard/utils/input_utils.py
+++ poudelard/utils/input_utils.py
@@ -1,9 +1,4 @@
-def demander_nom():
-    nom=str(input("Etrer le nom de votre personnage :"))
-    prénom=str(input("Etrer le prénom de votre personnage :"))
-    message='welcome to Poudlard',nom+' '+prénom
-    return message
-print(demander_nom())
+
 
 
 def demander_texte():
