88831cc7e882e3f5bcf49b69f92d223620045367
Romain9294 <romain.pinard@efrei.net>
2025-12-05 19:09:24 +0100

    modif 2eme fonction crée

--- poudelard/utils/input_utils.py
+++ poudelard/utils/input_utils.py
@@ -1,12 +1,39 @@
-def demander_texte(message):
+'''def demander_texte(message):
     while  True:
         texte = input(message).strip()
 
         if len(texte) == 0:
             print("Veuillez entrer un texte non vide.")
         else:
-            return texte
+            return texte'''
 
-nom = demander_texte("Entrez le nom de votre personnage : ")
-print("Bienvenue,", nom)
+def demander_nombre(message, min_val=None, max_val=None):
+    while True:
+        texte = input(message + " ").strip()
 
+        # Vérifier si la saisie est un nombre
+        est_valide = True
+        for c in texte:
+            if c < '0' or c > '9':
+                est_valide = False
+                break
+
+        if not est_valide:
+            print("Veuillez entrer uniquement des chiffres.")
+            continue
+
+        # Conversion manuelle
+        valeur = 0
+        for c in texte:
+            valeur = valeur * 10 + (ord(c) - ord('0'))
+
+        # Vérification des bornes
+        if min_val is not None and valeur < min_val:
+            print(f"Veuillez entrer un nombre >= {min_val}.")
+            continue
+
+        if max_val is not None and valeur > max_val:
+            print(f"Veuillez entrer un nombre <= {max_val}.")
+            continue
+
+        return valeur
