c3204637dd26238a719f73398d0c991c4054a364
Laurent <thetitan931@gmail.com>
2025-12-05 19:52:24 +0100

    personnage fini début chap 1 input_utils.py presque fini manque plus que 1

--- poudelard/univers/personnage.py
+++ poudelard/univers/personnage.py
@@ -1,32 +1,39 @@
-from poudelard.utils.input_utils import demander_nom
+def initialiser_personnage(prenom, nom, attributs):
 
-
-def initialiser_personnage(prenom,nom,argent,attributs):
-    return {
-        "prenom" : prenom,
-        "nom" : nom,
-        "argent" : argent,
-        "inventaire" : [],
-        "sortilèges" : [],
-        "attributs" : attributs
-
+    joueur = {
+        "nom": nom,
+        "prenom": prenom,
+        "argent": 100,
+        "inventaire": [],
+        "Sortilèges" : [],
+        "attributs": attributs
     }
+    return joueur
 
-def afficher_personnage(personnage):
-    print(f"Profil du personnage :")
-    print(f"Nom : {personnage['nom']}")
-    print(f"Prénom : {personnage['prenom']}")
-    print(f"Argent : {personnage['argent']}")
-    print(f"Inventaire : {personnage['inventaire']}")
-    print(f"Sortilèges : {personnage['sortilèges']}")
-    print(f"Attributs :")
-    print(f"    -Ambition : {personnage['attributs']['ambition']}")
-    print(f"    -Courage : {personnage['attributs']['courage']}")
-    print(f"    -Intelligence : {personnage['attributs']['intelligence']}")
-    print(f"    -Loyauté : {personnage['attributs']['loyauté']}")
+def afficher_personnage(joueur):
+    print("Profil du personnage : ")
+    print(f"Nom : {joueur['nom']}")
+    print(f"Prénom : {joueur['prenom']}")
+    print(f"Argent : {joueur['argent']}")
+    print(f"Inventaire : {joueur['inventaire']}")
+    print("Sortilèges :")
+    for sortilege in joueur["Sortilèges"]:
+        print(f"- {sortilege}")
+    print("Attributs :")
+    for attributs, valeur in joueur["attributs"].items():
+        print(f"- {attributs} : {valeur}")
 
 if __name__ == "__main__":
-    nom, prenom = demander_nom()
-    joueur = initialiser_personnage(prenom, nom, 100, 5)
-    afficher_personnage(joueur)
 
+    print("---Test Affichage du personnage---")
+    attributs_test = {"courage": 7, "ambition": 8, "intelligence": 9, "loyauté" : 10,}
+    joueur_test = initialiser_personnage("Harry", "Potter", attributs_test)
+    afficher_personnage(joueur_test)
+
+
+
+def modifier_argent(joueur, montant):
+    joueur["argent"] += montant
+
+def ajouter_objet_inventaire(joueur,cle, objet):
+    joueur[cle].append(objet)
