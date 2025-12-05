167716dd035fe81394b2c7bf5da9485b77147e52
mendo <hans.mendo@efrei.net>
2025-12-05 16:46:53 +0100

    correction fonction et premier test

--- poudelard/utils/input_utils.py
+++ poudelard/utils/input_utils.py
@@ -1,3 +1,8 @@
+def demander_texte(texte):
+    res=texte
+    return res
 
 
-def demander_texte():
+nom=str(input("Veuillez entrer votre nom:"))
+prénom=str(input("Veuillez entrer votre prénom:"))
+print(demander_texte(nom),demander_texte(prénom))
