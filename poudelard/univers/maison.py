5dd056d6c9c175a63c180b2420ed0ff6955d4914
Laurent <thetitan931@gmail.com>
2025-12-07 22:16:14 +0100

    tentative actualiser_points_maison et afficher

--- poudelard/univers/maison.py
+++ poudelard/univers/maison.py
@@ -1,37 +1,49 @@
-'''def actualiser_points_maison(maisons, nom_maison, points):
-    maisons = {
+maisons = {
         "Gryffondor": 0,
         "Serpentard": 0,
         "Poufsouffle": 0,
         "Serdaigle": 0,
     }
 
+def actualiser_points_maison(maisons, nom_maison, points):
     if nom_maison in maisons:
         maisons[nom_maison] += points
         print(f"La maison {nom_maison} a maintenant {maisons[nom_maison]} points.")
     else:
-        print(f"Erreur : la maison '{nom_maison}' est introuvable.")'''
+        print(f"Erreur : la maison '{nom_maison}' est introuvable.")
 
 
 def afficher_maison_gagnante(maisons):
     max_point = 0
-    maisons = {
-        "Gryffondor": 5,
-        "Serpentard": 8,
-        "Poufsouffle": 7,
-        "Serdaigle": 7,
-    }
-    for val in maisons.values():
-        if max_point < val:
-            max_point = val
-    for k, v in maisons.items():
-        if v == max_point:
-         print("la maison ayant le score le plus élevé est :", k)
-
+    for points in maisons.values():
+        if points > max_point:
+            max_point = points
+    maisons_gagnantes = []
+    for nom_maison, points in maisons.items():
+        if points == max_point:
+            maisons_gagnantes.append(nom_maison)
+    if len(maisons_gagnantes) == 1:
+        print(f"La maison gagnante est {maisons_gagnantes[0]} avec {max_point} points")
+    else :
+        liste_gagnantes = ", ".join(maisons_gagnantes)
+        print(f"Les maisons gagnantes sont : {liste_gagnantes} avec {max_point} points")
 
 
 if __name__ == "__main__":
-    maisons={}
-
-    print(afficher_maison_gagnante(maisons))
+    print("\n--- Test 1 : Vainqueur unique ---")
+    maisons_test_1 = {
+        "Gryffondor": 150,
+        "Serpentard": 120,
+        "Poufsouffle": 50,
+        "Serdaigle": 100
+    }
+    afficher_maison_gagnante(maisons_test_1)
+    print("\n--- Test 2 : Égalité parfaite ---")
+    maisons_test_2 = {
+        "Gryffondor": 200,
+        "Serpentard": 200,
+        "Poufsouffle": 50,
+        "Serdaigle": 100
+    }
+    afficher_maison_gagnante(maisons_test_2)
 
