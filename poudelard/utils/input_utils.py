14da3d01eb9b1f1909ad2ac8dfa470a9c40e2e5d
Romain9294 <romain.pinard@efrei.net>
2025-12-09 13:07:49 +0100

    fonction load fichier faite par mes soins BIS

--- poudelard/utils/input_utils.py
+++ poudelard/utils/input_utils.py
@@ -50,3 +50,10 @@ if __name__ == "__main__":
     resultat = demander_choix("Tu préfères quoi entre : ", mes_options)
     print(f"Tu préfères {mes_options[resultat - 1]}")
 
+    import json
+
+    def load_fichier(chemin_fichier):
+        with open(chemin_fichier, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+
