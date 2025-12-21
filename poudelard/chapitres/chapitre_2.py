c4fa8ca1a5ab6c0b557c01f661333b04e9f0661e
mendo <hans.mendo@efrei.net>
2025-12-21 02:05:02 +0100

    rencontré_amis terminé(il faut juste trouver comment ramener joueur chap1 dans chap2

--- poudelard/chapitres/chapitre_2.py
+++ poudelard/chapitres/chapitre_2.py
@@ -1,17 +1,19 @@
-from poudelard.chapitres.chapitre_1 import creer_personnage
 from poudelard.utils.input_utils import demander_choix
+from poudelard.chapitres.chapitre_1 import lancer_chapitre_1
+joueur= lancer_chapitre_1()
+
 
 def rencontrer_amis(joueur):
     for i in range(3):
         if i==0:
             options=[ "Bien sûr, assieds-toi !" , "Désolé, je préfère voyager seul."]
-            message = "que repondez vous ?:"
+            message = "que repondez vous ?:\n"
         elif i==1:
             options=[ "Oui, j’adore apprendre de nouvelles choses ! ","Euh… non, je préfère les aventures aux bouquins."]
-            message = "que repondez vous ?:"
+            message = "que repondez vous ?:\n"
         elif i==2:
             options=[". Je lui serre la main poliment.","Je l’ignore complètement.","Je lui réponds avec arrogance. "]
-            message = "comment réagissez vous ?:"
+            message = "comment réagissez vous ?:\n"
 
         list_narrateur=["Vous montez à bord du Poudlard Express. Le train démarre lentement en direction du Nord... Un garçon roux entre dans votre compartiment, l’air amical.\n","Une fille entre ensuite, portant déjà une pile de livres.\n","Puis un garçon blond entre avec un air arrogant.\n"]
         list_phrase=["-Salut ! Moi c’est Ron Weasley.Tu veux bien qu’on s’assoie ensemble ? ","-Bonjour, je m’appelle Hermione Granger. Vous avez déjà lu ‘Histoire de la Magie’ ?","-Je suis Drago Malefoy. Mieux vaut bien choisir ses amis dès le départ, tu ne crois pas"]
@@ -23,35 +25,36 @@ def rencontrer_amis(joueur):
            print('Ron sourit : — Génial ! Tu verras, Poudlard, c’est incroyable !(loyauté:+1)' )
           else:
            joueur['ambition']+=1
-           print(joueur)
+           print('Ron est malheureux: — désolé pour le dérangement')
         if i==1:
           choix = demander_choix(message, options)
           if choix == 1:
             joueur['intelligence'] += 1
-            print(joueur)
+            print('Hermione est ravi: — ca nous fait un point en commun ')
           else:
             joueur['courage'] += 1
-            print(joueur)
+            print("Hermione fronce les sourcils : — Il faudrait pourtant s’y mettre un jour ! ")
         if i==2:
           choix = demander_choix(message, options)
           if choix == 1:
             joueur['ambition'] += 1
-            print(joueur)
+            print('Drago souris: - Enchanté je me nomme Drago Malfoy')
           elif choix == 2:
             joueur['loyaute'] += 1
-            print(joueur)
+            print("Drago fronce les sourcils, vexé. — Tu le regretteras ! ")
           elif choix == 3:
             joueur['courage'] += 1
-            print(joueur)
-    return joueur
+            print('Drago est choqué: - comment oses tu!')
+    print('Le train continue sa route. Le château de Poudlard se profile à l’horizon... \nTes choix semblent déjà en dire long sur ta personnalité ! ')
+    print('Tes attributs mis à jour :', joueur)
 
 
 
 
-i=0
 
-options=[ "Bien sûr, assieds-toi !" , "Désolé, je préfère voyager seul."]
-joueur={'loyaute': 0, 'courage': 0, 'intelligence': 0, 'ambition': 0}
+
+
+
 rencontrer_amis(joueur)
 
 
